62193be091457c36000342ee1c0798d375b1a058
AnkitJ393 <jangidankit767@gmail.com>
2022-03-19 01:10:48 +0530

    Initial Commit Stuck at map of Rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoapp';
+  list:any[]=[] ;
+  constructor(){
+
+  }
+  addtask(task:string){
+    this.list.push({id:this.list.length,name:task})
+  }
+
+  delete(item:number){
+    // console.log(id)
+   this.list= this.list.filter(id=>id.id!=item);
+  }
 }
